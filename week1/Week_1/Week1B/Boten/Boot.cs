9d4126eebb1dfe932b1a3fc50c0773f982e9c8fa
Ernst Bolt <eb0095856@windesheim.nl>
2018-09-03 15:49:18 +0200

    pull

---
+++ week1/Week_1/Week1B/Boten/Boot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1B.Boten
+{
+    class Boot
+    {
+        public string Naam { get; set; }
+        public int Snelheid { get; set; }
+
+        public Boot(string naam)
+        {
+            Naam = naam;
+        }
+    }
+}
+
+
---
+++ week1/Week_1/Week1B/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week1B.Boten;
+
+namespace Week1B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boot b = new Boot();
+            b.Snelheid = -1;
+        }
+    }
+}
